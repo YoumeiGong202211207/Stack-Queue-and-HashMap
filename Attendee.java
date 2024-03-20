9de5384de298c23091827f40d8fbde1761bbff43
YoumeiGong202211207 <161431584+YoumeiGong202211207@users.noreply.github.com>
2024-03-20 16:25:52 +0800

    Add files via upload

---
+++ Attendee.java
@@ -0,0 +1,20 @@
+public class Attendee {
+    private String name;
+
+    public Attendee(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Attendee: " + name;
+    }
+}
